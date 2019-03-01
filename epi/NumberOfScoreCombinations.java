5379d85054b2d08b2f08b9d860a9ede314f70ca4
Lei <leiwang8224@gmail.com>
2019-02-28 20:59:29 -0800

    added comments

--- epi/NumberOfScoreCombinations.java
+++ epi/NumberOfScoreCombinations.java
@@ -1,46 +1,56 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 import java.util.List;
+
+// Write a program that takes a final score and scores for individual plays, and returns the number
+// of combinations of plays that result in the final score.
 public class NumberOfScoreCombinations {
-  @EpiTest(testDataFile = "number_of_score_combinations.tsv")
+    @EpiTest(testDataFile = "number_of_score_combinations.tsv")
 
-  public static int
-  numCombinationsForFinalScore(int finalScore,
-                               List<Integer> individualPlayScores) {
-    // TODO - you fill in here.
-    int[][] numCombos = new int[individualPlayScores.size()][finalScore+1];
-    for (int individualPlayScoresIndex = 0; individualPlayScoresIndex < individualPlayScores.size(); individualPlayScoresIndex++) {
-        final int FIRST_SCORE_INDEX = 0;
-        numCombos[individualPlayScoresIndex][FIRST_SCORE_INDEX] = 1; // one way to reach 0
-      for (int currentScore = 1; currentScore <= finalScore; currentScore++) {
-        int withoutThisPlay = 0;
-        int withThisPlay = 0;
+    public static int
+    numCombinationsForFinalScore(int finalScore,
+                                 List<Integer> individualPlayScores) {
+        // TODO - you fill in here.
+        int[][] numCombos = new int[individualPlayScores.size()][finalScore + 1];
 
-        if (individualPlayScoresIndex > 0) {
-            // just use the last score
-          withoutThisPlay = numCombos[individualPlayScoresIndex -1][currentScore];
+        // iterate through each individual score
+        for (int individualPlayScoresIndex = 0; individualPlayScoresIndex < individualPlayScores.size(); individualPlayScoresIndex++) {
+            final int FIRST_SCORE_INDEX = 0;
+            numCombos[individualPlayScoresIndex][FIRST_SCORE_INDEX] = 1; // one way to reach 0
+
+            // iterate through all possible scores up to the final score
+            for (int currentScore = 1; currentScore <= finalScore; currentScore++) {
+                int withoutThisPlay = 0;
+                int withThisPlay = 0;
+
+                if (individualPlayScoresIndex > 0) {
+                    // just use the last score[index - 1]
+                    withoutThisPlay = numCombos[individualPlayScoresIndex - 1][currentScore];
+                }
+
+                if (currentScore >= individualPlayScores.get(individualPlayScoresIndex)) {
+                    // if the current score is above the individual score
+                    // subtract individual score from current score
+                    withThisPlay = numCombos[individualPlayScoresIndex][currentScore - individualPlayScores.get(individualPlayScoresIndex)];
+                }
+
+                // current score is scores added from without this play and with this play
+                numCombos[individualPlayScoresIndex][currentScore] = withoutThisPlay + withThisPlay;
+            }
         }
-
-        if (currentScore >= individualPlayScores.get(individualPlayScoresIndex)) {
-            // if the current score is above the individual score
-            // subtract individual score from current score
-          withThisPlay = numCombos[individualPlayScoresIndex][currentScore - individualPlayScores.get(individualPlayScoresIndex)];
-        }
-
-        // current score is scores added from without this play and with this play
-        numCombos[individualPlayScoresIndex][currentScore] = withoutThisPlay + withThisPlay;
-      }
-    }
-    return numCombos[individualPlayScores.size() - 1][finalScore];
+        return numCombos[individualPlayScores.size() - 1][finalScore];
 //    return 0;
-  }
+    }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "NumberOfScoreCombinations.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "NumberOfScoreCombinations.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
--- epi/SumRootToLeaf.java
+++ epi/SumRootToLeaf.java
@@ -1,19 +1,37 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+// Consider a binary tree in which each node contains a binary digit. A root to leaf path
+// can be associated with a binary number. The MSB at the root. Design an algorithm
+// to compute the sum of the binary numbers represented by the root to leaf paths.
+
 public class SumRootToLeaf {
-  @EpiTest(testDataFile = "sum_root_to_leaf.tsv")
+    @EpiTest(testDataFile = "sum_root_to_leaf.tsv")
 
-  public static int sumRootToLeaf(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    public static int sumRootToLeaf(BinaryTreeNode<Integer> tree) {
+        return sumUpFromRoot(tree, 0);
+    }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "SumRootToLeaf.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    private static int sumUpFromRoot(BinaryTreeNode<Integer> tree, int partialSum) {
+        if (tree == null) return 0;
+
+        // for each digit of 1's, multiply the current sum by 2 and add current node data
+        partialSum = partialSum * 2 + tree.data;
+        if (tree.left == null && tree.right == null)
+            return partialSum;
+
+        return sumUpFromRoot(tree.left, partialSum) + sumUpFromRoot(tree.right, partialSum);
+
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "SumRootToLeaf.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
